7e749ebf830d670681d9d6daadefe875c9658f03
Ilgizar Ismakaev <ilgizar3332@gmail.com>
2021-09-29 12:43:43 +0300

    second exercise

--- Homework_1/homework_1.js
+++ Homework_1/homework_1.js
@@ -7,3 +7,17 @@ function translate() {
     console.log('Некорректный ввод!');
   }
 }
+
+function numbers() {
+  let value1 = parseInt(prompt(), 10);
+  if (isNaN(value1)) {
+    console.log('Некорректный ввод!');
+  } else {
+    let value2 = parseInt(prompt(), 10);
+    if (isNaN(value2)) {
+      console.log('Некорректный ввод!');
+    } else {
+      console.log(`Ответ: ${value1 + value2}, ${value1 / value2}.`);
+    }
+  }
+}
