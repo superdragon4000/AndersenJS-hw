22e43264a01de08fd916e385b43cfa66f972d99e
Ilgizar Ismakaev <ilgizar3332@gmail.com>
2021-10-07 21:53:17 +0500

    fix hw3

--- Homework_3/homework_3.js
+++ Homework_3/homework_3.js
@@ -1,9 +1,15 @@
-Array.prototype.myFilter = function (callback) {
+Array.prototype.myFilter = function (callback, thisArg) {
   let filteredArray = [];
   for(let i = 0; i < this.length; i++) {
+    if (thisArg !== undefined) {
+      if(callback(thisArg[i], i, thisArg)) {
+        filteredArray.push(this[i]);
+      }  
+    } else {
       if(callback(this[i], i, this)) {
-          filteredArray.push(this[i]);
+        filteredArray.push(this[i]);
       }
+    }
   }
   return filteredArray;
 }
